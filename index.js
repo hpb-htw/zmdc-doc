6ddd5145f7682c56c9cb8a20119cbdbed339a7e2
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2025-04-13 11:02:26 +0200

    auto commit

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ import Prism from 'prismjs';
 
 window.Prism = Prism;
 
-document.addEventListener("DOMContentLoaded", async () => {
-    demoUsageTextFormatFunction();
-    showDemoCode('index.js');
-});
-
+// tag: howto-show-demo-code
+//{
+    document.addEventListener("DOMContentLoaded", async () => {
+        demoUsageTextFormatFunction();
+        await showDemoCode('index.js');
+        verifyContentOfFanyDemo();
+    });
+//}
 
 function formatFancy(text) {
     const escaped = htmlEscape(text);
@@ -28,14 +31,69 @@ function demoUsageTextFormatFunction() {
     document.getElementById("ff-result").innerHTML = fancy;
 }
 
+// tag: howto-prepare-demo-function
+function howToPrepareDemoFunction () {
+    function demoUsageTextFormatFunction() {
+        const text = "Funny texts don't need Comic Sans";
+        const fancy = formatFancy(text);
+        // <span id="ff-result"></span>
+        document.getElementById("ff-result").innerHTML = fancy;
+    }
+    // <span id="ff-result"></span>
+}
+
+// tag: howto-run-demo-function
+function howtoRunDemoFunction() {
+    demoUsageTextFormatFunction();
+}
+
+// tag: howto-mark-demo-info
+function howtoMarkDemoInfo() {
+    // tag: fancy-demo
+    function demoUsageTextFormatFunction() {
+        const text = "Funny texts don't need Comic Sans";
+        const fancy = formatFancy(text);
+        document.getElementById("ff-result").innerHTML = fancy;
+        // <span id="ff-result">
+    }
+    // <div id="fancy-demo">
+    //     <pre><code class="example-javascript"></code><pre>
+    //     <pre><code class="example-html"></code></pre>
+    // </div>
+}
+
+
 /**
  * this function can also load any other demo script.
  * */
-async function showDemoCode(demoScript) {
-    const response = await fetch(demoScript, {method: 'GET'});
-    const text = await response.text();
-    const demoExamples = parseExampleFunctions(text);
-    for(const example of demoExamples) {
-        showExampleCode(example);
+// tag: howto-load-demo-code
+//{
+    async function showDemoCode(demoScript) {
+        const response = await fetch(demoScript, {method: 'GET'});
+        const text = await response.text();
+        const demoExamples = parseExampleFunctions(text);
+        for (const example of demoExamples) {
+            showExampleCode(example);
+        }
     }
+//}
+
+// tag: verify-contents-of-fancy-demo
+function verifyContentOfFanyDemo() {
+    const id = "fancy-demo";
+    const verifyId = "duplicate-fancy-demo";
+
+    function showCode(bySelector) {
+        const div = document.getElementById(id);
+        const code = div.querySelector(bySelector).innerHTML;
+        const verify = document.getElementById(verifyId);
+        verify.querySelector(bySelector).innerText = code;
+    }
+
+    // <div id="duplicate-fancy-demo">
+    //    <pre><code class="example-javascript language-javascript"></code></pre>
+    //    <pre><code class="example-html language-html"></code></pre>
+    // </div>
+    showCode(JS_EXAMPLE_EL_QUERY);
+    showCode(HTML_EXAMPLE_EL_QUERY);
 }
